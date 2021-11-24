aa642eaff9156a0d4435fd2d62d7f03f28c6f567
vctriusss <vskamaev@miem.hse.ru>
2021-11-24 20:02:13 +0300

    improvements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import numpy as np
-
+from math import log10
 
 # данная функция разбивает сообщение в 2 СС на блоки длиной lsb
 # Каждый блок (двоичное число) переводим в десятичное
@@ -40,9 +40,9 @@ def main():
     task = input('Что вы хотите сделать:\n'
                  '1) Встроить сообщение\n'
                  '2) Извлечь сообщение\n'
-                 '3) Выход\n')
+                 '3) Рассчитать пиковое соотношение сигнал-шум для 2 изображений\n')
     if task == 3:
-        return 0
+        check_quality()
     name0 = input('Введите имя изначального изображения в формате "name.format"\n')
     global name
     img = Image.open(name0)
@@ -65,11 +65,14 @@ def encrypt(img):
     print('Введите информацию для встраивания в файл input.txt')
     lsb = int(input('Введите количество LSB, которые можно изменять (от 1 до 7):\n'))
     with open('input.txt', 'r', encoding='utf-8') as f:
-        msg_normal = f.read()
+        msg_normal = f.read()[:400000]
     msg = msg_normal if msg_type == '2' else text_to_bin(msg_normal)
     bin_len = bin(len(msg))[2:]
     msg = '0' * (24 - len(bin_len)) + bin_len + msg  # к сообщению добавляем его длину
     size = len(msg)
+    if size / (3 * lsb * width * height) > 1:
+        print('Невозможно провести встраивание. Размер сообщения слишком большой')
+        return -1
     bin_lsb = bin(lsb)[2:]
     bin_lsb = '0' * (3 - len(bin_lsb)) + bin_lsb  # формируем число кол-во lsb
     msg = refactor_message(msg, lsb)
@@ -91,9 +94,8 @@ def encrypt(img):
     img.close()
     print('Встраивание завершено успешно!\nПолученное изображение находится в той же папке '
           + f'под именем {name}1.bmp')
-    print(f'Емкость встраивания: {3 * lsb} бит сообщения (около {round(3 * lsb / 11, 3)} символа) на 1 пиксель '
-          f'изображения')
-    print(f'Заполненность контейнера: {round((2 + size / (3 * lsb)) * 100 / (width * height), 4)}%')
+    print(f'Емкость встраивания: {len(msg) / (width * height)} бит/пиксель')
+    print(f'Заполненность контейнера: {round(size * 100 / (3 * lsb * width * height), 4)}%')
 
 
 def decrypt(img):
@@ -106,7 +108,7 @@ def decrypt(img):
     msg_type = str(pixels[3] % 2)   # считываем тип сообщения
     bin_len = ''
     for color in pixels[4: 23 // lsb + 5]:
-        bin_len += bin(color)[-lsb:]   # считываем длину сообщения
+        bin_len += ('0' * (8 - len(bin(color)[2:])) + bin(color)[2:])[-lsb:]   # считываем длину сообщения
     if len(bin_len) > 24:
         # если считали как длину кусок, длиной больше 24 битов, то этот остаток есть начало сообщения
         msg += bin_len[24: len(bin_len)]
@@ -114,7 +116,7 @@ def decrypt(img):
     size = int(bin_len, 2)
     delta_msg = len(msg)
     for color in pixels[23 // lsb + 5: 23 // lsb + 6 + (size - delta_msg - 1) // lsb]:
-        msg += bin(color)[-lsb:]   # считываем сообщение
+        msg += ('0' * (8 - len(bin(color)[2:])) + bin(color)[2:])[-lsb:]   # считываем сообщение
     if len(msg) > size:
         msg = msg[:size]   # обрезаем лишние биты справа
     msg = msg if msg_type == '0' else bin_to_text(msg)  # преобразуем полученную битовую последовательность в сообщение
@@ -124,4 +126,25 @@ def decrypt(img):
     img.close()
 
 
+def check_quality():
+    name1 = input('Введите имя первого изображения в формате "name.bmp"\n')
+    name2 = input('Введите имя первого изображения в формате "name.bmp"\n')
+    img1 = Image.open(name1)
+    img2 = Image.open(name2)
+    width, height = img1.size
+    pixels1 = np.asarray(img1).reshape((width * height, 3))
+    pixels2 = np.asarray(img2).reshape((width * height, 3))
+    summ = 0
+    for (pixel1, pixel2) in zip(pixels1, pixels2):
+        r1, g1, b1 = pixel1
+        r2, g2, b2 = pixel2
+        y1 = 0.3 * r1 + 0.59 * g1 + 0.11 * b1
+        y2 = 0.3 * r2 + 0.59 * g2 + 0.11 * b2
+        summ += (y1 - y2) ** 2
+    mse = summ / (width * height)
+    psnr = 10 * log10((255 ** 2) / mse)
+    print('PSNR =', round(psnr), 'dB')
+    img1.close()
+    img2.close()
 main()
+
